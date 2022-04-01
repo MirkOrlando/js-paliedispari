e4c2ec025647de595432042b899d4a56d16cefa3
MirkOrlando <mirko@orlando-m.it>
2022-04-01 17:48:41 +0200

    created form .palindrome

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,36 @@ Chiedere all’utente di inserire una parola.
 Creare una funzione per capire se la parola inserita è palindroma
  */
 
+const formPalindromeElement = document.querySelector(".palindrome form");
+const btnSubmitPalindromeElement = document.querySelector(
+  ".palindrome .submit"
+);
+
+formPalindromeElement.addEventListener("submit", function (event) {
+  event.preventDefault();
+  btnSubmitPalindromeElement.setAttribute("disabled", "disabled");
+  const text = document.getElementById("text").value;
+  const readResultPalindromeElement = document.querySelector(
+    ".palindrome div.read_result"
+  );
+  if (isPalindrome(text)) {
+    readResultPalindromeElement.innerHTML = `La parola -${text.toUpperCase()}- è palindroma`;
+  } else {
+    readResultPalindromeElement.innerHTML = `La parola -${text.toUpperCase()}- non è palindroma`;
+  }
+});
+
+formPalindromeElement.addEventListener("reset", function (event) {
+  event.preventDefault();
+  btnSubmitPalindromeElement.removeAttribute("disabled", "disabled");
+  const text = document.getElementById("text");
+  const readResultPalindromeElement = document.querySelector(
+    ".palindrome div.read_result"
+  );
+  text.value = "";
+  readResultPalindromeElement.innerHTML = "";
+});
+
 /**
  * Returns a boolean value that indicates whether a string is palindrome
  * @param {string} word A string value
@@ -18,27 +48,15 @@ function isPalindrome(string) {
   }
 }
 
-/* const word = prompt("type a word");
-console.log(word);
-if (isPalindrome(word)) {
-  console.log(`La parola -${word.toUpperCase()}- è palindroma`);
-} else {
-  console.log(`La parola -${word.toUpperCase()}- non è palindroma`);
-}
- */
-
 /* Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
- */
+*/
 
 const formGameElement = document.querySelector(".game form");
-const submitGameElement = document.querySelector(".game submit");
-const resetGameElement = document.querySelector(".game reset");
-
 formGameElement.addEventListener("submit", function (event) {
   event.preventDefault();
   /* L’utente sceglie pari o dispari */
@@ -62,22 +80,32 @@ formGameElement.addEventListener("submit", function (event) {
   console.log(userChoice, userNumber, pcNumber, sum);
 
   /* Dichiariamo chi ha vinto. */
-  const readResultElement = document.querySelector(".game div.read_result");
+  const readResultGameElement = document.querySelector(".game div.read_result");
   if (userChoice === "default") {
-    readResultElement.innerHTML = `Prima è necessario scegliere pari o dispari!`;
+    readResultGameElement.innerHTML = `Prima è necessario scegliere pari o dispari!`;
   } else if (userNumber < 1 || userNumber > 5) {
-    readResultElement.innerHTML = `Selezionare un numero compreso tra 1 e 5!`;
+    readResultGameElement.innerHTML = `Selezionare un numero compreso tra 1 e 5!`;
   } else {
     switch (result) {
       case userChoiceEven:
-        readResultElement.innerHTML = `il tuo numero è ${userNumber},<br>il numero del computer è ${pcNumber},<br>tu hai scelto ${userChoice}, quindi...<br>User wins!`;
+        readResultGameElement.innerHTML = `il tuo numero è ${userNumber},<br>il numero del computer è ${pcNumber},<br>tu hai scelto ${userChoice}, quindi...<br>User wins!`;
         break;
 
       default:
-        readResultElement.innerHTML = `il tuo numero è ${userNumber},<br>il numero del computer è ${pcNumber},<br>tu hai scelto ${userChoice}, quindi...<br>PC wins!`;
+        readResultGameElement.innerHTML = `il tuo numero è ${userNumber},<br>il numero del computer è ${pcNumber},<br>tu hai scelto ${userChoice}, quindi...<br>PC wins!`;
         break;
     }
   }
+});
+
+formGameElement.addEventListener("reset", function (event) {
+  const userChoice = document.getElementById("even_or_odd");
+  const userNumber = parseInt(document.getElementById("user_number"));
+  const readResultGameElement = document.querySelector(".game div.read_result");
+  event.preventDefault();
+  userNumber.value = "";
+  userChoice.value = "default";
+  readResultGameElement.innerHTML = "";
 });
 
 /**
