4305a56a632af5c16cb9ee7c3ff48072cde4b425
MirkOrlando <mirko@orlando-m.it>
2022-04-01 17:20:22 +0200

    created form pari o dispari

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,37 +35,50 @@ Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
  */
 
-/* L’utente sceglie pari o dispari */
-const userChoice = prompt('Scegli: "pari" o "dipari"?');
-let userChoiceEven = false;
-if (userChoice.toLowerCase() === "pari") {
-  userChoiceEven = true;
-} else if (userChoice.toLowerCase() === "dispari") {
-  userChoiceEven = false;
-} else {
-  alert('Cortesemente digitare "pari" o "dispari"');
-}
-/* e inserisce un numero da 1 a 5. */
-const userNumber = parseInt(prompt("Digita un numero da 1 a 5"));
-/* Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). */
-const pcNumber = getRndInteger(1, 5);
-/* Sommiamo i due numeri */
-const sum = userNumber + pcNumber;
-/* Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione). */
-const result = isEven(sum);
+const formGameElement = document.querySelector(".game form");
+const submitGameElement = document.querySelector(".game submit");
+const resetGameElement = document.querySelector(".game reset");
 
-console.log(userChoice, userNumber, pcNumber, sum);
+formGameElement.addEventListener("submit", function (event) {
+  event.preventDefault();
+  /* L’utente sceglie pari o dispari */
+  const userChoice = document.getElementById("even_or_odd").value;
+  let userChoiceEven = false;
+  if (userChoice === "pari") {
+    userChoiceEven = true;
+  } else if (userChoice === "dispari") {
+    userChoiceEven = false;
+  }
 
-/* Dichiariamo chi ha vinto. */
-switch (result) {
-  case userChoiceEven:
-    console.log("User wins");
-    break;
+  /* e inserisce un numero da 1 a 5. */
+  const userNumber = parseInt(document.getElementById("user_number").value);
+  /* Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). */
+  const pcNumber = getRndInteger(1, 5);
+  /* Sommiamo i due numeri */
+  const sum = userNumber + pcNumber;
+  /* Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione). */
+  const result = isEven(sum);
 
-  default:
-    console.log("PC wins");
-    break;
-}
+  console.log(userChoice, userNumber, pcNumber, sum);
+
+  /* Dichiariamo chi ha vinto. */
+  const readResultElement = document.querySelector(".game div.read_result");
+  if (userChoice === "default") {
+    readResultElement.innerHTML = `Prima è necessario scegliere pari o dispari!`;
+  } else if (userNumber < 1 || userNumber > 5) {
+    readResultElement.innerHTML = `Selezionare un numero compreso tra 1 e 5!`;
+  } else {
+    switch (result) {
+      case userChoiceEven:
+        readResultElement.innerHTML = `il tuo numero è ${userNumber},<br>il numero del computer è ${pcNumber},<br>tu hai scelto ${userChoice}, quindi...<br>User wins!`;
+        break;
+
+      default:
+        readResultElement.innerHTML = `il tuo numero è ${userNumber},<br>il numero del computer è ${pcNumber},<br>tu hai scelto ${userChoice}, quindi...<br>PC wins!`;
+        break;
+    }
+  }
+});
 
 /**
  * Returns a boolean value that indicates whether a number is even
